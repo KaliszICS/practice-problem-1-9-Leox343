aa4370d8a99cb594a419043f5dd196bf8bedccf2
Leox343 <leo.xu6@student.tdsb.on.ca>
2024-09-30 17:45:31 +0000

    h

--- PP1_9.py
+++ PP1_9.py
@@ -1,31 +1,36 @@
-
+'''
+lesson 1.9 
+created on sep 30
+last edited on sep 30
+'''
 
 def q1(): 
   #Write Assignment code here
-
-
+  print('"Hello World"')
 def q2(): 
   #Write Assignment code here
-
-
+  ans = input("Input a word: ")
+  print(ans.lower())
+  print(ans.upper())
 def q3(): 
   #Write Assignment code here
-
-
+  ans1 = input("Input a word that is at least 5 letters long: ")
+  print(ans1[1:4])
 def q4(): 
   #Write Assignment code here
-
-
+  ans2 = input("Input a word: ")
+  print(ans2.index("o"))
 def q5(): 
   #Write Assignment code here
-
+  ans3 = input("Input a word: ")
+  print(len(ans3))
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+# q1()
+# q2()
+# q3()
+# q4()
+# q5()
